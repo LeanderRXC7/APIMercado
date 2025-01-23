0d804a522af0e0650cc572b90791c5e4286cf93a
Leander Campos <leanderrxcampos@gmail.com>
2025-01-23 13:27:11 -0300

    Start

---
+++ mercado/src/test/java/br/cefet/mercado/MercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cefet.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
